7951b7abdeb20e243be977cb2e9bb174a8d374ef
Veeraraghhav178 <veeraraghavulu08@gmail.com>
2024-12-01 23:11:17 +0530

    added socket io

--- src/_metronic/partials/layout/header-menus/HeaderUserMenu.tsx
+++ src/_metronic/partials/layout/header-menus/HeaderUserMenu.tsx
@@ -133,3 +133,257 @@ const HeaderUserMenu: FC = () => {
 }
 
 export {HeaderUserMenu}
+
+
+
+// import React, { useEffect, useRef } from "react";
+// import {
+//   Button,
+//   Card,
+//   Col,
+//   Container,
+//   Dropdown,
+//   DropdownButton,
+//   Row,
+// } from "react-bootstrap";
+// import { useParams } from "react-router-dom";
+// import ApiAxios from "../../modules/auth/core/ApiAxios";
+// import "./Editpage.scss";
+// import { useAuth } from "../../modules/auth";
+// import { ToasterPage } from "../../modules/shared/Toaster/toaster";
+// import Accordion from 'react-bootstrap/Accordion';
+// import { fabric } from "fabric";
+// import ListGroup from 'react-bootstrap/ListGroup';
+
+
+// export const EditPage = () => {
+//   const { id } = useParams();
+//   const { auth } = useAuth();
+//   const colRef: any = useRef(null);
+//   const [loading, setIsLoading] = React.useState(false);
+//   const [composits, setComposits] = React.useState<any>([]);
+//   const [layers, setLayers] = React.useState<any>([]);
+//   const [show, setShow] = React.useState(false);
+//   const [msg, setMsg] = React.useState("");
+//   const [selectedLayer, setSelectedLayer] = React.useState<any>({})
+//   const canvasRef = useRef(null);
+//   const fabricCanvasRef: any = useRef(null);
+//   const [selectedColor, setSelectedColor] = React.useState('#fff')
+
+//   // Initialize Fabric.js Canvas
+//   useEffect(() => {
+//     getComposits()
+//     fabricCanvasRef.current = new fabric.Canvas(canvasRef.current);
+    
+//     // Optionally set some properties for the canvas
+//     fabricCanvasRef.current.setWidth(800);
+//     fabricCanvasRef.current.setHeight(600);
+    
+//     // Clear the canvas on component unmount
+//     return () => {
+//       fabricCanvasRef.current.dispose();
+//     };
+//   }, [colRef]);
+
+//   const getComposits = () => {
+//     ApiAxios.get('/composits/').then((resp: any) => {
+//       setComposits(resp.data.results)
+//     }, (error) => {
+//       console.log(error)
+//     })
+//   }
+
+//   // Add Circle
+//   const addCircle = () => {
+//     const circle = new fabric.Circle({
+//       radius: 50,
+//       fill: "red",
+//       left: 100,
+//       top: 100,
+//     });
+//     fabricCanvasRef.current.add(circle);
+//   };
+
+//   // Add Rectangle
+//   const addRectangle = () => {
+//     const rect = new fabric.Rect({
+//       width: 100,
+//       height: 60,
+//       fill: "blue",
+//       left: 150,
+//       top: 150,
+//     });
+//     fabricCanvasRef.current.add(rect);
+//   };
+
+//   // Add Text
+//   const addText = (layer?: any, color?: any) => {
+//     const text = new fabric.Textbox(layer.text, {
+//       left: 10,
+//       top: 10,
+//       fontSize: 24,
+//       fill: color,
+//     });
+//     fabricCanvasRef.current.add(text);
+//   };
+
+//   // Add Image
+//   const addImage = (layer?: any) => {
+//     const imageUrl = layer?.mainsrc;
+//     if (imageUrl) {
+//       fabric.Image.fromURL(imageUrl, (img) => {
+//         // Set image properties (position, scale, etc.)
+//         img.set({
+//           left: 50,
+//           top: 50,
+//           scaleX: 1.2,
+//           scaleY: 1.5,
+//           width: 800,
+//           height: 650,
+//           selectable: false,
+//         });
+
+//         // Add the image to the fabric canvas
+//         fabricCanvasRef.current.add(img);
+//         fabricCanvasRef.current.renderAll(); // Re-render the canvas
+//       });
+//     } else {
+//       alert('No image selected');
+//     }
+//   };
+
+//   const handleSave = () => {
+//     const obj = {
+//       templaate_state: "Edited",
+//       order_id: id,
+//       user: auth?.id,
+//       main_template: 2,
+//     };
+//     ApiAxios.post("/mytemplates/", obj).then(
+//       (resp: any) => {
+//         setShow(true);
+//         setMsg("Saved successfully");
+//       },
+//       (error: any) => console.log(error)
+//     );
+//   };
+
+//   // Handle image upload
+//   const handleImageUpload = (event: any) => {
+//     const file = event.target.files[0];
+//     if (file) {
+//       const reader = new FileReader();
+//       reader.onload = (e: any) => {
+//         setSelectedLayer({ mainsrc: e.target.result });
+//       };
+//       reader.readAsDataURL(file);
+//     }
+//   };
+
+//   const handleItem = (layer: any) => {
+//     setSelectedLayer(layer)
+//     if(layer.type === 'text') {
+//       addText(layer, '#000')
+//     } else if(layer.type === 'image') {
+//       addImage(layer)
+//     }
+//   }
+
+//   const handleChangeColor = (e: any) => {
+//     const newColor = e.target.value;
+//     setSelectedColor(newColor); // Update the state with the new color
+
+//     // Optionally update all text objects if you want to change the color of all text at once
+//     const activeObject = fabricCanvasRef.current.getActiveObject();
+//     if (activeObject && activeObject.type === "textbox") {
+//       activeObject.set({ fill: newColor });
+//       fabricCanvasRef.current.renderAll(); // Re-render to apply the new color
+//     }
+//   }
+
+//   const handleComposit = (composit: any) => {
+//     ApiAxios.get(`/layers/?compositid=${composit.id}`).then((resp: any) => {
+//       setLayers(resp.data.results)
+//       resp.data.results.map((t: any) => {
+//         if(t.type === 'image') {
+//           addImage(t)
+//         } else if(t.type === 'text') {
+//           addText(t, '#fff')
+//         }
+//       })
+      
+//     }, (error) => {console.log(error)})
+//   }
+//   console.log('layersss', layers)
+//   return (
+//     <div>
+//       <Row>
+//         <Col lg={8} md={12} ref={colRef}>
+//           <div className="icon-container">
+//             <img src="/media/images/text.png" onClick={addText} />
+//             <img src="/media/images/image (2).png" />
+//             <img src="/media/images/rectangle.png" onClick={addRectangle} />
+//             <img src="/media/images/circle.png" onClick={addCircle} />
+//             <input type="color" onChange={handleChangeColor} />
+//           </div>
+//           <canvas ref={canvasRef} id="fabricCanvas" />
+//         </Col>
+//         <Col lg={4} md={12}>
+//           <Card>
+//             <Card.Body>
+//               <Card.Text>
+//                 <div className="d-flex justify-content-between mt-10">
+//                   <div>
+//                     <DropdownButton
+//                       variant="secondary"
+//                       title={"Select Composit"}
+//                       id="dropdown-basic"
+//                     >
+//                       <Dropdown.Menu>
+//                         <Dropdown.Item
+//                         // key={item.id}
+//                         // onClick={() => handleSelectCategory(item.id)} // Set category on click
+//                         >
+//                           Composite 1
+//                         </Dropdown.Item>
+//                       </Dropdown.Menu>
+//                     </DropdownButton>
+//                   </div>
+//                   <div className="d-flex gap-4">
+//                     <Button variant="secondary" onClick={() => alert("")}>
+//                       Render
+//                     </Button>
+//                     <Button variant="secondary" onClick={handleSave}>
+//                       Save
+//                     </Button>
+//                   </div>
+//                 </div>
+//                 <div className="my-12">
+//                   <Accordion defaultActiveKey="0">
+//                     {composits.map((item: any, ind: any) => 
+//                       <Accordion.Item eventKey={ind}>
+//                       <Accordion.Header onClick={() => handleComposit(item)}>{item.composit_name}</Accordion.Header>
+//                       <Accordion.Body>
+//                       <ListGroup>
+//                         {layers.map((item: any, ind: any) => 
+//                           <ListGroup.Item className="text-white cursor-pointer my-1 bg-body-secondary"  onClick={() => handleItem(item)}>
+//                             <img src={`/media/images/${item.type === 'text' ? 'text' : item.type === 'image' ? 'image (2)' : ''}.png`} width={15} /> {item.name}
+//                           </ListGroup.Item>
+//                         )}
+//                         </ListGroup>
+//                       </Accordion.Body>
+//                     </Accordion.Item>
+//                     )}
+//                   </Accordion>
+//                   </div>
+//               </Card.Text>
+//             </Card.Body>
+//           </Card>
+//         </Col>
+//       </Row>
+//       <ToasterPage show={show} setShow={setShow} toastMsg={msg} />
+//     </div>
+//   );
+// };
+
+
--- src/app/pages/editPage/EditPage.tsx
+++ src/app/pages/editPage/EditPage.tsx
@@ -1,255 +1,3 @@
-// import React, { useEffect, useRef } from "react";
-// import {
-//   Button,
-//   Card,
-//   Col,
-//   Container,
-//   Dropdown,
-//   DropdownButton,
-//   Row,
-// } from "react-bootstrap";
-// import { useParams } from "react-router-dom";
-// import ApiAxios from "../../modules/auth/core/ApiAxios";
-// import "./Editpage.scss";
-// import { useAuth } from "../../modules/auth";
-// import { ToasterPage } from "../../modules/shared/Toaster/toaster";
-// import Accordion from 'react-bootstrap/Accordion';
-// import { fabric } from "fabric";
-// import ListGroup from 'react-bootstrap/ListGroup';
-
-
-// export const EditPage = () => {
-//   const { id } = useParams();
-//   const { auth } = useAuth();
-//   const colRef: any = useRef(null);
-//   const [loading, setIsLoading] = React.useState(false);
-//   const [composits, setComposits] = React.useState<any>([]);
-//   const [layers, setLayers] = React.useState<any>([]);
-//   const [show, setShow] = React.useState(false);
-//   const [msg, setMsg] = React.useState("");
-//   const [selectedLayer, setSelectedLayer] = React.useState<any>({})
-//   const canvasRef = useRef(null);
-//   const fabricCanvasRef: any = useRef(null);
-//   const [selectedColor, setSelectedColor] = React.useState('#fff')
-
-//   // Initialize Fabric.js Canvas
-//   useEffect(() => {
-//     getComposits()
-//     fabricCanvasRef.current = new fabric.Canvas(canvasRef.current);
-    
-//     // Optionally set some properties for the canvas
-//     fabricCanvasRef.current.setWidth(800);
-//     fabricCanvasRef.current.setHeight(600);
-    
-//     // Clear the canvas on component unmount
-//     return () => {
-//       fabricCanvasRef.current.dispose();
-//     };
-//   }, [colRef]);
-
-//   const getComposits = () => {
-//     ApiAxios.get('/composits/').then((resp: any) => {
-//       setComposits(resp.data.results)
-//     }, (error) => {
-//       console.log(error)
-//     })
-//   }
-
-//   // Add Circle
-//   const addCircle = () => {
-//     const circle = new fabric.Circle({
-//       radius: 50,
-//       fill: "red",
-//       left: 100,
-//       top: 100,
-//     });
-//     fabricCanvasRef.current.add(circle);
-//   };
-
-//   // Add Rectangle
-//   const addRectangle = () => {
-//     const rect = new fabric.Rect({
-//       width: 100,
-//       height: 60,
-//       fill: "blue",
-//       left: 150,
-//       top: 150,
-//     });
-//     fabricCanvasRef.current.add(rect);
-//   };
-
-//   // Add Text
-//   const addText = (layer?: any, color?: any) => {
-//     const text = new fabric.Textbox(layer.text, {
-//       left: 10,
-//       top: 10,
-//       fontSize: 24,
-//       fill: color,
-//     });
-//     fabricCanvasRef.current.add(text);
-//   };
-
-//   // Add Image
-//   const addImage = (layer?: any) => {
-//     const imageUrl = layer?.mainsrc;
-//     if (imageUrl) {
-//       fabric.Image.fromURL(imageUrl, (img) => {
-//         // Set image properties (position, scale, etc.)
-//         img.set({
-//           left: 50,
-//           top: 50,
-//           scaleX: 1.2,
-//           scaleY: 1.5,
-//           width: 800,
-//           height: 650,
-//           selectable: false,
-//         });
-
-//         // Add the image to the fabric canvas
-//         fabricCanvasRef.current.add(img);
-//         fabricCanvasRef.current.renderAll(); // Re-render the canvas
-//       });
-//     } else {
-//       alert('No image selected');
-//     }
-//   };
-
-//   const handleSave = () => {
-//     const obj = {
-//       templaate_state: "Edited",
-//       order_id: id,
-//       user: auth?.id,
-//       main_template: 2,
-//     };
-//     ApiAxios.post("/mytemplates/", obj).then(
-//       (resp: any) => {
-//         setShow(true);
-//         setMsg("Saved successfully");
-//       },
-//       (error: any) => console.log(error)
-//     );
-//   };
-
-//   // Handle image upload
-//   const handleImageUpload = (event: any) => {
-//     const file = event.target.files[0];
-//     if (file) {
-//       const reader = new FileReader();
-//       reader.onload = (e: any) => {
-//         setSelectedLayer({ mainsrc: e.target.result });
-//       };
-//       reader.readAsDataURL(file);
-//     }
-//   };
-
-//   const handleItem = (layer: any) => {
-//     setSelectedLayer(layer)
-//     if(layer.type === 'text') {
-//       addText(layer, '#000')
-//     } else if(layer.type === 'image') {
-//       addImage(layer)
-//     }
-//   }
-
-//   const handleChangeColor = (e: any) => {
-//     const newColor = e.target.value;
-//     setSelectedColor(newColor); // Update the state with the new color
-
-//     // Optionally update all text objects if you want to change the color of all text at once
-//     const activeObject = fabricCanvasRef.current.getActiveObject();
-//     if (activeObject && activeObject.type === "textbox") {
-//       activeObject.set({ fill: newColor });
-//       fabricCanvasRef.current.renderAll(); // Re-render to apply the new color
-//     }
-//   }
-
-//   const handleComposit = (composit: any) => {
-//     ApiAxios.get(`/layers/?compositid=${composit.id}`).then((resp: any) => {
-//       setLayers(resp.data.results)
-//       resp.data.results.map((t: any) => {
-//         if(t.type === 'image') {
-//           addImage(t)
-//         } else if(t.type === 'text') {
-//           addText(t, '#fff')
-//         }
-//       })
-      
-//     }, (error) => {console.log(error)})
-//   }
-//   console.log('layersss', layers)
-//   return (
-//     <div>
-//       <Row>
-//         <Col lg={8} md={12} ref={colRef}>
-//           <div className="icon-container">
-//             <img src="/media/images/text.png" onClick={addText} />
-//             <img src="/media/images/image (2).png" />
-//             <img src="/media/images/rectangle.png" onClick={addRectangle} />
-//             <img src="/media/images/circle.png" onClick={addCircle} />
-//             <input type="color" onChange={handleChangeColor} />
-//           </div>
-//           <canvas ref={canvasRef} id="fabricCanvas" />
-//         </Col>
-//         <Col lg={4} md={12}>
-//           <Card>
-//             <Card.Body>
-//               <Card.Text>
-//                 <div className="d-flex justify-content-between mt-10">
-//                   <div>
-//                     <DropdownButton
-//                       variant="secondary"
-//                       title={"Select Composit"}
-//                       id="dropdown-basic"
-//                     >
-//                       <Dropdown.Menu>
-//                         <Dropdown.Item
-//                         // key={item.id}
-//                         // onClick={() => handleSelectCategory(item.id)} // Set category on click
-//                         >
-//                           Composite 1
-//                         </Dropdown.Item>
-//                       </Dropdown.Menu>
-//                     </DropdownButton>
-//                   </div>
-//                   <div className="d-flex gap-4">
-//                     <Button variant="secondary" onClick={() => alert("")}>
-//                       Render
-//                     </Button>
-//                     <Button variant="secondary" onClick={handleSave}>
-//                       Save
-//                     </Button>
-//                   </div>
-//                 </div>
-//                 <div className="my-12">
-//                   <Accordion defaultActiveKey="0">
-//                     {composits.map((item: any, ind: any) => 
-//                       <Accordion.Item eventKey={ind}>
-//                       <Accordion.Header onClick={() => handleComposit(item)}>{item.composit_name}</Accordion.Header>
-//                       <Accordion.Body>
-//                       <ListGroup>
-//                         {layers.map((item: any, ind: any) => 
-//                           <ListGroup.Item className="text-white cursor-pointer my-1 bg-body-secondary"  onClick={() => handleItem(item)}>
-//                             <img src={`/media/images/${item.type === 'text' ? 'text' : item.type === 'image' ? 'image (2)' : ''}.png`} width={15} /> {item.name}
-//                           </ListGroup.Item>
-//                         )}
-//                         </ListGroup>
-//                       </Accordion.Body>
-//                     </Accordion.Item>
-//                     )}
-//                   </Accordion>
-//                   </div>
-//               </Card.Text>
-//             </Card.Body>
-//           </Card>
-//         </Col>
-//       </Row>
-//       <ToasterPage show={show} setShow={setShow} toastMsg={msg} />
-//     </div>
-//   );
-// };
-
-
-
 import React, { useEffect, useState } from 'react';
 import {
   Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField, Button, Collapse, IconButton
@@ -259,6 +7,8 @@ import ApiAxios from '../../modules/auth/core/ApiAxios';
 import { Badge } from 'react-bootstrap';
 import { useAuth } from '../../modules/auth';
 import { ToasterPage } from '../../modules/shared/Toaster/toaster';
+import { io } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
 
 interface RowData {
   id: number;
@@ -285,9 +35,10 @@ export const EditPage = () => {
     // 2: [{ id: 1, product: 'Tablet', quantity: 3 }, { id: 2, product: 'Monitor', quantity: 1 }],
   });
 
+  const socket = io('http://3.88.186.149:5000/'); // Point to your server's URL
   // State for expanded row and editing row
   const { auth } = useAuth();
-  console.log('authhhhh', auth)
+  const { id } = useParams();
   const [expandedRow, setExpandedRow] = useState<number | null>(null);
   const [editingNestedRow, setEditingNestedRow] = useState<number | null>(null);
   const [composits, setComposits] = useState<any>(null)
@@ -307,7 +58,6 @@ export const EditPage = () => {
     })
   }
 
-console.log('composiiii', layers)
 
 const getLayers = (row: any) => {
   ApiAxios.get(`/layers/?compositid=${row.id}`).then((resp: any) => {
@@ -386,9 +136,33 @@ const getLayers = (row: any) => {
     // }));
   };
 
+  const handleRender = (type: any, cid: any) => {
+    let message = "";
+    if(type === 'template') {
+      message = `templete_${id}@user_${auth?.id}`
+    } else {
+      message = `templete_${id}@composit_${cid}@user_${auth?.id}`
+    }
+    socket.emit('message', message);
+  }
+
   return (
     <div className='edit-table-page'>
-      <h3>Composits</h3>
+      <h3 className='m-5'>Composits
+        <span style={{float: 'right', display: 'flex', gap: 10}}>
+        <Button variant="contained"
+          color="primary"
+          size="small">
+            Save
+        </Button>
+        <Button variant="contained"
+          color="primary"
+          onClick={() => handleRender('template', '')}
+          size="small">
+            Render
+        </Button>
+        </span>
+      </h3>
       <TableContainer style={{padding: 40}}>
       <Table>
         <TableHead>
@@ -418,6 +192,10 @@ const getLayers = (row: any) => {
                   <IconButton onClick={() => handleToggleExpand(row)}>
                     {expandedRow === row.id ? <ExpandLess /> : <ExpandMore />}
                   </IconButton>
+                  <Button variant="contained"
+                                    color="primary"
+                                    onClick={() => handleRender('composit', row.id)}
+                                    size="small">Render</Button>
                 </TableCell>
               </TableRow>
 
